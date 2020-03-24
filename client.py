e2a8061ea254dcfc60b5834e621ad6f471035e3d
Matthew <1matthew779@gmail.com>
2020-03-23 20:23:28 -0700

    update

--- client.py
+++ client.py
@@ -1,24 +1,41 @@
 import socket
 import sys
+from tkinter import *
+import time
 
 
-port = 6000
+class client:
 
-#server ip address
-ip = '167.99.160.18'
-
-ADDR = (ip,port)
-
-#TCP socket for making connection
-tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-try:
-    tcp_socket.connect(ADDR)
-    
+    PORT = 6011
+    ip = '192.168.1.9'
+    ADDR = (ip,PORT)
 
 
-#UDP socket for data transmitting
-#udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    def __init__(self,frame):
+        self.frame = frame
+        self.GUI()
+        self.tcp_connection()
+       
+    #simple GUI for testing
+    def GUI(self):
+        self.message = Message(self.frame,text = "this is a message", width = 200)
+        self.message.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
+
+    def tcp_connection(self):
+        self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.tcp_socket.connect(self.ADDR)
+        
+       # while True:
+        m = self.tcp_socket.recv(4096)
+        m = m.decode('utf-8')   
+        print(m)
+        self.message.configure(text = m)
 
 
-    
+
+
+
+frame = Tk()
+
+client(frame)
+frame.mainloop()
--- server.py
+++ server.py
@@ -6,22 +6,38 @@ import threading
 
 class server:
     
-    PORT = 6001
+    PORT = 6011
     ADDR = ('',PORT)
-
+    clients = []
+    Size = 0
     def __init__(self):
-        self.clients = {}
         self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.tcp_socket.bind(self.ADDR)
         self.tcp_socket.listen(5)
 
         threading.Thread(target=self.tcp_connection).start()
 
+        self.print_list()
+
+
+    def print_list(self):
+        
+        while self.Size <2:
+            i = 1
+
+        for c in self.clients:
+            print(c)
+
     #TCP socket for making connection
     def tcp_connection(self):
         while True:
-            c , addr = self.tcp_socket.accept()
+            c  = self.tcp_socket.accept()
+            addr = c[1]
+            self.clients.append(addr)
+            self.Size += 1
             print("Get connection from: ", addr)
+            m = ("welcome to chat room").encode('utf-8')
+            c[0].send(m)
 
 
 server()
