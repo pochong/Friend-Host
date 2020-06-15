b8b32f0b7090eb9e7c30adb3d2b0d39746080a86
Matthew <1matthew779@gmail.com>
2020-06-14 20:30:05 -0700

    Updated

--- client.py
+++ client.py
@@ -6,89 +6,180 @@ import cv2
 import mss
 import numpy as np
 from PIL import ImageTk, Image
+import cv2
+import pickle
+import struct
+import threading
 
 
 class client:
 
-    PORT = 6011
+    PORT = 8004
     ip = '192.168.1.9'
     ADDR = (ip,PORT)
-
+    username = "User1"
 
     def __init__(self,frame):
         self.frame = frame
         self.GUI()
         self.tcp_connection()
+
+
+    #start to capture screen HOWEVER IT DOESNT WORK !!!!
+    def startStream(self):
+        self.scene = mss.mss()
+        self.screen = {"top": 40, "left": 0,"width":1000,"height":900}
+        self.readscreen_ms = scene.grab(self.screen)
+        self.readscreen_np = np.array(self.readscreen_ms)
+        self.readscreen = Image.fromarray(self.readscreen_np)
+        self.readscreen_tk = ImageTk.PhotoImage(image = readscreen)
+        self.showscreen = Label(mainframe, image = self.readscreen_tk)               
+        self.showscreen.pack()
+        print("HI")
        
+    #function for text entry displays in chat room
+    def sendText(self):
+
+        mes = bytes(self.text_entry.get(), 'utf-8')
+        try:
+            self.tcp_socket.send(mes)
+            print("Message Sent")
+        except:
+            print("Failed to send message")
+        """
+        try:
+            response = self.tcp_socket.recv(4096)
+            if(response == b'received'): 
+                print("response received")
+        except:
+            print("Failed to receive Response")
+        """
+        self.chat_room.insert(END, self.username + " : " + self.text_entry.get() + "\n")
+        self.text_entry.delete(0, END)
+
+    def update_chat_room(self):
+        #TODO: 
+        while True: 
+            try:
+                mes = self.tcp_socket.recv(4096)
+                s = mes.decode("utf-8")
+                self.chat_room.insert(END, "User : " + s + "\n")
+                self.text_entry.delete(0, END)
+            except: 
+                print("Failed to update chat room")
+        
+
+
+    #Initialization of all the functionalities. 
+    def initialize(self): 
+        #Initialize a thread to wait for messages from other clients.
+        threading.Thread(target=self.update_chat_room).start()
+
+
     #simple GUI for testing
     def GUI(self):
-        self.message = Message(self.frame,text = "this is a message", width = 200)
-        self.message.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
         
-        #the Left frame of the GUI 
-        mainframe = Frame(self.frame, width = 500, height = 380, bg = 'lightgrey')
-        mainframe.grid(row = 0, column = 0, padx = 5, pady = 5)
-
-        #start to capture screen HOWEVER IT DOESNT WORK !!!!
-        def startStream():
-            scene = mss.mss()
-            screen = {"top": 40, "left": 0,"width":1000,"height":900}
-            readscreen_ms = scene.grab(screen)
-            readscreen_np = np.array(readscreen_ms)
-            readscreen = Image.fromarray(readscreen_np)
-            readscreen_tk = ImageTk.PhotoImage(image = readscreen)
-            showscreen = Label(mainframe, image = readscreen_tk)               
-            showscreen.pack()
-            print("HI")
+        self.Display = Label(self.frame, height=50,width = 200)
+        self.Display.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
         
         #the Right frame of the GUI
-        subframe = Frame(self.frame, width = 170, height = 380, bg = 'lightgrey')
-        subframe.grid(row = 0, column = 1, padx = 5, pady = 5)
+        self.subframe = Frame(self.frame, width = 170, height = 380, bg = 'lightgrey')
+        self.subframe.grid(row = 0, column = 1, padx = 5, pady = 5)
 
         #Chat room Text display
-        chat_room = Text(subframe, width = 30, height = 19)
-        chat_room.grid(row = 0, column = 0, padx = 5, pady = 5)
+        self.chat_room = Text(self.subframe, width = 30, height = 19)
+        self.chat_room.grid(row = 0, column = 0, padx = 5, pady = 5)
 
-        text_entry = Entry(subframe, width = 30)
-        text_entry.grid(row = 1, column = 0, padx = 5, pady =5)
-        
-        #function for text entry displays in chat room
-        def sendText(self):
-            chat_room.insert(END, "User : " + text_entry.get() + "\n")
-            text_entry.delete(0, END)
+        self.text_entry = Entry(self.subframe, width = 30)
+        self.text_entry.grid(row = 1, column = 0, padx = 5, pady =5)   
         
         #press ENTER to send message
-        text_entry.bind('<Return>', sendText)
+        self.text_entry.bind('<Return>', self.sendText)
 
         #send button to send / show message in chat room
-        send_btn = Button(subframe, text = 'Send', width = 8, command=sendText)
-        send_btn.grid(row = 2, column = 0, padx = 5, pady = 5)
+        self.send_btn = Button(self.subframe, text = 'Send', width = 8, command=self.sendText)
+        self.send_btn.grid(row = 2, column = 0, padx = 5, pady = 5)
 
         #buttons frame to put buttons
-        buttons_frame = Frame(self.frame, width = 500, height = 50, bg = 'lightgrey')
-        buttons_frame.grid(row = 1, column = 0, padx = 10, pady = 0)
+        self.buttons_frame = Frame(self.frame, width = 500, height = 50, bg = 'lightgrey')
+        self.buttons_frame.grid(row = 1, column = 0, padx = 10, pady = 0)
 
         #start button
-        start_btn = Button(buttons_frame, text = 'Start', width=8, command = startStream)
-        start_btn.grid(row = 0, column = 0, padx = 5, pady = 5)
+        self.start_btn = Button(self.buttons_frame, text = 'Start', width=8, command = self.startStream)
+        self.start_btn.grid(row = 0, column = 0, padx = 5, pady = 5)
 
         #stop button
-        stop_btn = Button(buttons_frame, text = 'Stop', width=8)
-        stop_btn.grid(row = 0, column = 1, padx = 5, pady = 5)
+        self.stop_btn = Button(self.buttons_frame, text = 'Stop', width=8)
+        self.stop_btn.grid(row = 0, column = 1, padx = 5, pady = 5)
 
         #mute button
-        mute_btn = Button(buttons_frame, text = 'Mute', width=8)
-        mute_btn.grid(row = 0, column = 2, padx = 5, pady = 5)
+        self.mute_btn = Button(self.buttons_frame, text = 'Mute', width=8)
+        self.mute_btn.grid(row = 0, column = 2, padx = 5, pady = 5)
+    
+    
         
+    
+    def read_screen(self):
+        ms = mss.mss()
+        screen = {"top": 40, "left": 0,"width":1000,"height":900}
+        while True:
+            #capture screen pixel
+            frame = ms.grab(screen)
+            #cap = cv2.VideoCapture(0)
+            #ret,frame = cap.read()
+            #convert pixel to matrix and show it on cv2 
+            #cv2.imshow('screen', np.array(frame))
+            #print(readscreen_pil)
+            #press q to quit
+
+            data = pickle.dumps(frame) ### new code
+            self.tcp_socket.sendall(struct.pack("L", len(data))+data) ### new code
+
+            
+            if cv2.waitKey(25) & 0XFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+            
+
+    def recv_screen(self):
+        data = b''
+        payload_size = struct.calcsize("L") 
+        while True:
+            while len(data) < payload_size:
+                data += self.tcp_socket.recv(4096)
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += self.tcp_socket.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            
+            ###
+            
+            frame=pickle.loads(frame_data)
+            f2 = open('frame1.jpg','wb')
+            f2.write(frame)
+            #print frame
+            #cv2.imshow('frame',np.array(frame))
+    
+
     def tcp_connection(self):
         self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.tcp_socket.connect(self.ADDR)
         
        # while True:
         m = self.tcp_socket.recv(4096)
-        m = m.decode('utf-8')   
-        print(m)
-        self.message.configure(text = m)
+        if m == b'host':
+            #start a thread to send camera frame
+            print("Go to read screen")
+            #self.read_screen()
+        else:
+            print("Go to recv screen")
+            #self.recv_screen()
+        threading.Thread(target=self.initialize).start()
+
 
 
     
--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 import socket
 import sys
 import threading
-
+import pickle
+import struct
 
 
 class server:
     
-    PORT = 6011
+    PORT = 8004
     ADDR = ('',PORT)
-    clients = []
+    clients_address = []
+    clients_socket = []
     Size = 0
+
+    #d = b''
     def __init__(self):
         self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.tcp_socket.bind(self.ADDR)
@@ -25,19 +29,84 @@ class server:
         while self.Size <2:
             i = 1
 
-        for c in self.clients:
+        for c in self.clients_address:
             print(c)
 
-    #TCP socket for making connection
+    """
+    def recv_screen(self):
+        print("in recv_screen")
+        data = b''
+        payload_size = struct.calcsize("L") 
+        while True:
+            while len(data) < payload_size:
+                data += self.clients_socket[0].recv(4096)
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += self.clients_socket[0].recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            if self.Size > 1:
+
+                d = pickle.dumps(frame_data) ### new code
+                self.clients_socket[1].sendall(struct.pack("L", len(d))+d) ### new code
+            
+            ###
+            
+            frame=pickle.loads(frame_data)
+            print frame
+            cv2.imshow('framd = pickle.dumps(frame_data) ### new code
+            self.tcp_socket.sendall(struct.pack("H", len(d))+d) ### new codeonnection
+            
+        """
+
+    def recv_message(self,num):
+        try: 
+            while(True):
+                mes = self.clients_socket[num].recv(4096)
+                """
+                try: 
+                    self.clients_socket[num].send(b'received')
+                except:
+                    print("Failed to reply ")
+                """
+                self.send_message(num,mes)
+        except: 
+            print("Failed to receive message")
+        
+    def send_message(self,num,mes):
+        try: 
+            i = 0
+            while(i < self.Size) :
+                if(i!=num): 
+                    self.clients_socket[i].send(mes)
+                i += 1
+        except: 
+            print("Failed to send message ")
+
     def tcp_connection(self):
         while True:
             c  = self.tcp_socket.accept()
             addr = c[1]
-            self.clients.append(addr)
+            self.clients_address.append(addr)
+            self.clients_socket.append(c[0])
+            
+            if self.Size == 0:
+                c[0].send(b'host')
+               # threading.Thread(target = self.recv_message,args = (self.Size)).start()
+            else:
+                c[0].send(b'client')
+            threading.Thread(target = self.recv_message,args = (self.Size,)).start()
             self.Size += 1
+
+            
+            """
             print("Get connection from: ", addr)
             m = ("welcome to chat room").encode('utf-8')
             c[0].send(m)
+            """
 
 
 server()
